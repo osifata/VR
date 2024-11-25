9697d4dd78a09d4bb05dd3bb952fb9972e5fcd12
Sofa <sonyabuzevich@gmail.com>
2024-11-25 14:20:42 +0800

    переход от катсцены на игру

--- pompeii/catscene.gd
+++ pompeii/catscene.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var move = $AnimationPlayer2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$AnimationPlayer2.play("move")
+	await move.animation_finished
+	get_tree().call_deferred("change_scene_to_file", "res://world.tscn")
